c926e7c2b827393c7ca04ff1ca3fbd5ac6c9ac32
LEE DONG GYU <tenenger7125@gmail.com>
2023-06-05 18:35:49 +0900

    refactor: use Title, Container of common components

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,14 +1,16 @@
 import { TodoInput, TodoList } from "../components/Todo";
+import { Container, Title } from "../components/common";
 import { useTodo } from "../hooks";
 
 const Todo = () => {
   const { todos, handleCreateTodo, handleUpdateTodo, handleDeleteTodo } = useTodo();
 
   return (
-    <>
+    <Container width="600px">
+      <Title>원티드 프리온보딩 사전과제</Title>
       <TodoInput handleCreateTodo={handleCreateTodo} />
       <TodoList {...{ todos, handleUpdateTodo, handleDeleteTodo }} />
-    </>
+    </Container>
   );
 };
 
