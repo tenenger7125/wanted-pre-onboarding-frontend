6b827b0e44300c86273ab4a5970204e9b7b9203b
LEE DONG GYU <tenenger7125@gmail.com>
2023-06-09 10:10:55 +0900

    refactor: update testid property sort

--- src/constants/testid.js
+++ src/constants/testid.js
@@ -2,14 +2,14 @@ const TEST_ID = {
   EMAIL_INPUT: "email-input",
   PASSWORD_INPUT: "password-input",
   MODIFY_INPUT: "modify-input",
+  NEW_TODO_INPUT: "new-todo-input",
   SIGNUP_BUTTON: "signup-button",
   SIGNIN_BUTTON: "signin-button",
-  NEW_TODO_INPUT: "new-todo-input",
-  NEW_TODO_ADD_BUTTON: "new-todo-add-button",
   MODIFY_BUTTON: "modify-button",
   DELETE_BUTTON: "delete-button",
   SUBMIT_BUTTON: "submit-button",
   CANCEL_BUTTON: "cancel-button",
+  NEW_TODO_ADD_BUTTON: "new-todo-add-button",
 };
 
 export default TEST_ID;
